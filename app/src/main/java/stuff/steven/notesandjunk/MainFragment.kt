b40a86fa59f3e46dd31b6932972189526bfb7934
Steven Kallas <emailstevenkallas@gmail.com>
2018-05-16 16:20:48 -0600

    File writing and reading each is asynced

--- app/src/main/java/stuff/steven/notesandjunk/MainFragment.kt
+++ app/src/main/java/stuff/steven/notesandjunk/MainFragment.kt
@@ -1,22 +1,24 @@
 package stuff.steven.notesandjunk
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
 import android.view.*
+import android.widget.EditText
 import kotlinx.android.synthetic.main.fragment_main.*
+import java.lang.ref.WeakReference
 
 class MainFragment : Fragment() {
-
-    private val defaultPrefs by lazy {
-        PreferenceManager.getDefaultSharedPreferences(activity)
-    }
+    private lateinit var defaultPrefs: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        defaultPrefs = PreferenceManager.getDefaultSharedPreferences(activity)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -37,48 +39,76 @@ class MainFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         fetchText()
-        fetchCursorPosition()
     }
 
     private fun saveText(){
         val textToSave = edit_text.text.toString()
-        context?.openFileOutput(FILENAME_STORING_TEXT, Context.MODE_PRIVATE).use {
-            it?.write(textToSave.toByteArray())
-        }
-    }
-
-    private fun fetchText(){
-        val savedTextSize = defaultPrefs.getString(resources.getString(R.string.pref_key_text_size), resources.getString(R.string.pref_text_size_default))
-        val textColor = defaultPrefs.getString(resources.getString(R.string.pref_key_text_color), resources.getString(R.string.pref_text_color_default))
-        val backgroundColor = defaultPrefs.getString(resources.getString(R.string.pref_key_background_color), resources.getString(R.string.pref_background_color_default))
-
-        val file = context?.getFileStreamPath(FILENAME_STORING_TEXT)
-        var textToShow = ""
-        if (file?.exists() == true) {
-            context?.openFileInput(FILENAME_STORING_TEXT).use {
-                val bytes = it?.readBytes()
-                if (bytes != null) textToShow = String(bytes)
-            }
-        }
-
-        with(edit_text){
-            setTextColor(Color.parseColor(textColor))
-            setBackgroundColor(Color.parseColor(backgroundColor))
-            textSize = savedTextSize.toFloat()
-            setText(textToShow)
-            setSelection(fetchCursorPosition())
-        }
+        TextSaver(activity).execute(textToSave)
     }
 
     private fun saveCursorPosition() {
         with(defaultPrefs.edit()){
             putInt(resources.getString(R.string.pref_key_cursor_position), edit_text.selectionEnd)
-            commit()
+            apply()
         }
     }
 
-    private fun fetchCursorPosition() : Int{
-        return defaultPrefs.getInt(resources.getString(R.string.pref_key_cursor_position), 0)
+    private fun fetchText(){
+        TextFetcher(activity, edit_text, defaultPrefs).execute()
+        val savedTextSize = defaultPrefs.getString(resources.getString(R.string.pref_key_text_size), resources.getString(R.string.pref_text_size_default))
+        val textColor = defaultPrefs.getString(resources.getString(R.string.pref_key_text_color), resources.getString(R.string.pref_text_color_default))
+        val backgroundColor = defaultPrefs.getString(resources.getString(R.string.pref_key_background_color), resources.getString(R.string.pref_background_color_default))
+
+        with(edit_text){
+            setTextColor(Color.parseColor(textColor))
+            setBackgroundColor(Color.parseColor(backgroundColor))
+            textSize = savedTextSize.toFloat()
+        }
+    }
+
+    private class TextSaver(context: Context?) : AsyncTask<String, Void, Void>() {
+        private val weakContext = WeakReference(context)
+
+        override fun doInBackground(vararg params: String): Void? {
+            val textToSave = params[0]
+            this.weakContext.get()?.openFileOutput(FILENAME_STORING_TEXT, Context.MODE_PRIVATE).use {
+                it?.write(textToSave.toByteArray())
+            }
+            return null
+        }
+    }
+
+    private class TextFetcher(c: Context?, et: EditText, sp: SharedPreferences) : AsyncTask<Void, Void, String>() {
+        private val weakContext = WeakReference(c)
+        private val weakEditText = WeakReference(et)
+        private val weakSharedPrefs = WeakReference(sp)
+
+        override fun doInBackground(vararg params: Void?): String {
+            val context = this.weakContext.get()
+            val file = context?.getFileStreamPath(FILENAME_STORING_TEXT)
+            var textToShow = ""
+            if (file?.exists() == true) {
+                context.openFileInput(FILENAME_STORING_TEXT).use {
+                    val bytes = it?.readBytes()
+                    if (bytes != null) textToShow = String(bytes)
+                }
+            }
+            return textToShow
+        }
+
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+
+            with(weakEditText.get() as EditText){
+                setText(result)
+                setSelection(fetchCursorPosition())
+            }
+
+        }
+
+        private fun fetchCursorPosition() : Int{
+            return weakSharedPrefs.get()?.getInt(weakContext.get()?.resources?.getString(R.string.pref_key_cursor_position), 0)!!
+        }
     }
 
     companion object {
