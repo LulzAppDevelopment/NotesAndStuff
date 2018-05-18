56ee8b215ae9d0bcfcb3d11b415f1479888dc444
Steven Kallas <emailstevenkallas@gmail.com>
2018-05-17 19:00:09 -0600

    Soft keyboard hidden when settings menu opens.

--- app/src/main/java/stuff/steven/notesandjunk/SettingsFragment.kt
+++ app/src/main/java/stuff/steven/notesandjunk/SettingsFragment.kt
@@ -1,9 +1,12 @@
 package stuff.steven.notesandjunk
 
 import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.PreferenceFragmentCompat
+import android.view.inputmethod.InputMethodManager
+
 
 class SettingsFragment : PreferenceFragmentCompat(){
 
@@ -11,10 +14,14 @@ class SettingsFragment : PreferenceFragmentCompat(){
         super.onAttach(context)
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         //(activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        if (activity?.currentFocus != null) {
+            val inputMethodManager = activity?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(activity?.currentFocus!!.windowToken, 0)
+        }
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
-
 }
